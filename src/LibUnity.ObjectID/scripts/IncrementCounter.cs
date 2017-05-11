30f064d29486818be09a901a327012a90e902ec3
maayalee <maayalee@gmail.com>
2017-05-11 22:27:17 +0900

    Remove repo

--- src/LibUnity.ObjectID/scripts/IncrementCounter.cs
+++ src/LibUnity.ObjectID/scripts/IncrementCounter.cs
@@ -7,7 +7,12 @@ namespace LibUnity.ObjectID {
     }
 
     protected override uint GetCurrentTime() {
-      return Convert.ToUInt32(Time.GetUnixtimestamp());
+      return Convert.ToUInt32(GetUnixtimestamp());
+    }
+
+    private double GetUnixtimestamp() {
+      TimeSpan time = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0);
+      return time.TotalSeconds;
     }
   }
 }
